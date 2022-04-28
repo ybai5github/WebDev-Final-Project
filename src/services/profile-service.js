1d3b98afadcc97c600dc7da5dfbae0d4ff330e29
Felix <nampanya.f@northeastern.edu>
2022-04-28 13:07:55 -0400

    edited profile-service

--- src/services/profile-service.js
+++ src/services/profile-service.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
-const API_BASE = 'https://shrouded-brushlands-00973.herokuapp.com/editProfile';
+const API_BASE = 'http://localhost:4000/editProfile';
 
-const updateUser = async (user) => {
+export const updateUser = async (user) => {
   const response = await axios.put(`${API_BASE}/${user._id}`,user);
   return response.data;
 }
-
-export default updateUser;
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+  updateUser
+}
