5bb0293d088f618ee134f26c0b11ee7be417a0af
Felix <nampanya.f@northeastern.edu>
2022-04-28 13:08:42 -0400

    edit profile-service

--- src/services/profile-service.js
+++ src/services/profile-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:4000/editProfile';
+const API_BASE = 'https://shrouded-brushlands-00973.herokuapp.com/editProfile';
 
 export const updateUser = async (user) => {
   const response = await axios.put(`${API_BASE}/${user._id}`,user);
